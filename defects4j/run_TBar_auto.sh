9dae65d5d0de321319325e92dcc22eae69017a47
gmldbs <gmldbs3445@gmail.com>
2021-04-06 15:57:07 +0900

    To fit new verison
    To fit new verison

--- defects4j/run_TBar_auto.sh
+++ defects4j/run_TBar_auto.sh
@@ -1,11 +1,11 @@
-python kinds.py | while read line
+python3 kinds.py | while read line
 do
 #bug_info=($line)
 #   defects4j checkout -p ${bug_info[0]} -v ${bug_info[1]}b -w /TBar/D4J/projects/${bug_info[0]}_${bug_info[1]}
 #   cd /TBar/D4J/projects/${bug_info[0]}_${bug_info[1]}
 #   defects4j compile
 #   defects4j fault-localization -w /TBar/D4J/projects/${bug_info[0]}_${bug_info[1]}
-#   python /defects4j/preprocess.py /TBar/D4J/projects/${bug_info[0]}_${bug_info[1]}/sfl/txt/line.ochiai.ranking.csv ${bug_info[0]}_${bug_info[1]}
+#   python3 /defects4j/preprocess.py /TBar/D4J/projects/${bug_info[0]}_${bug_info[1]}/sfl/txt/line.ochiai.ranking.csv ${bug_info[0]}_${bug_info[1]}
 #   cd /TBar
 #   mkdir /Result
 #   ./NormalFLTBarRunner.sh /TBar/D4J/projects/ ${bug_info[0]}_${bug_info[1]} /TBar/D4J/defects4j/ > /Result/${bug_info[0]}_${bug_info[1]}
@@ -14,9 +14,9 @@ do
     cd /TBar/D4J/projects/Chart_8
     defects4j compile
     defects4j fault-localization -w /TBar/D4J/projects/Chart_8
-    python /defects4j/preprocess.py /TBar/D4J/projects/Chart_8/sfl/txt/line.ochiai.ranking.csv Chart_8
+    python3 /defects4j/preprocess.py /TBar/D4J/projects/Chart_8/sfl/txt/line.ochiai.ranking.csv Chart_8
     cd /TBar
-    mkdir /Result
-    ./NormalFLTBarRunner.sh /TBar/D4J/projects/ Chart_8 /TBar/D4J/defects4j/ > /Result/Chart_8
+    mkdir /defects4j/TBarResult
+    ./NormalFLTBarRunner.sh /TBar/D4J/projects/ Chart_8 /TBar/D4J/defects4j/ > /defects4j/TBarResult/Chart_8
     break
 done
